dc421c0de8320e7da821448a794d4629af83a6c8
Ian Parker <ian@geekprojects.com>
2019-10-28 17:45:44 +0000

    Styles: Add StyleEngine and CSS Parser

---
+++ src/libfrontier/styles/css3BaseListener.cpp
@@ -0,0 +1,7 @@
+
+// Generated from css3.g4 by ANTLR 4.7.2
+
+
+#include "css3BaseListener.h"
+
+
---
+++ src/libfrontier/styles/styleengine.cpp
@@ -0,0 +1,191 @@
+
+#include <string.h>
+#include <wchar.h>
+#include <time.h>
+
+#include <frontier/styles.h>
+#include <frontier/widgets.h>
+#include "cssparser.h"
+
+using namespace std;
+using namespace Frontier;
+using namespace Geek;
+
+#define STRINGIFY(x) XSTRINGIFY(x)
+#define XSTRINGIFY(x) #x
+
+StyleEngine::StyleEngine() : Logger("StyleEngine")
+{
+    m_parser = new CssParser(this);
+}
+
+StyleEngine::~StyleEngine()
+{
+}
+
+bool StyleEngine::init()
+{
+#ifdef CSSDIR
+    m_parser->parse(string(STRINGIFY(CSSDIR)) + "/frontier.css");
+#endif
+
+    return true;
+}
+
+bool StyleEngine::parse(std::string path)
+{
+    m_parser->parse(path);
+
+    m_timestamp = time(NULL);
+    return true;
+}
+
+StyleRule* StyleEngine::findByKey(std::string key)
+{
+    return NULL;
+}
+
+void StyleEngine::addRule(StyleRule* rule)
+{
+    m_styleRules.push_back(rule);
+}
+
+map<string, int64_t> StyleEngine::getProperties(Widget* widget)
+{
+    map<string, int64_t> results;
+    wstring classes = L"";
+    bool comma = false;
+    for (wstring clazz : widget->getWidgetClasses())
+    {
+        if (comma)
+        {
+            classes += L",";
+        }
+        comma = true;
+
+        classes += clazz;
+
+    }
+    log(DEBUG, "getProperties: Widget: type=%ls, classes=%ls, id=%ls", widget->getWidgetName().c_str(), classes.c_str(), widget->getWidgetId().c_str());
+
+map<StyleRule*, int> matchedRules;
+    for (StyleRule* rule : m_styleRules)
+    {
+
+        bool matchId = (rule->getId().length() > 0 && rule->getId() == widget->getWidgetId());
+        if (rule->getId().length() > 0 && !matchId)
+        {
+            continue;
+        }
+
+        bool matchType = (rule->getWidgetType().length() > 0 && (rule->getWidgetType() == L"Widget" || rule->getWidgetType() == widget->getWidgetName()));
+        if (rule->getWidgetType().length() > 0 && !matchType)
+        {
+            continue;
+        }
+
+        bool matchClass = (rule->getClassName().length() > 0 && widget->hasWidgetClass(rule->getClassName()));
+        if (rule->getClassName().length() > 0 && !matchClass)
+        {
+            continue;
+        }
+
+        bool matchState = false;
+        if (rule->getState().length() > 0)
+        {
+            matchState = true;
+            if (rule->getState() == "hover")
+            {
+                if (!widget->isMouseOver())
+                {
+                    continue;
+                }
+            }
+            else if (rule->getState() == "active")
+            {
+                if (!widget->isActive())
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                continue;
+            }
+        }
+
+        int specificity = 0;
+        if (matchId)
+        {
+            specificity += 1000;
+        }
+        if (matchState)
+        {
+            specificity += 100;
+        }
+        if (matchClass)
+        {
+            specificity += 10;
+        }
+        if (matchType)
+        {
+            if (rule->getWidgetType() == L"Widget")
+            {
+                specificity += 1;
+            }
+            else
+            {
+                specificity += 2;
+            }
+        }
+        matchedRules.insert(make_pair(rule, specificity));
+    }
+
+    matchedRules.insert(make_pair(widget->getWidgetStyle(), 10000));
+
+    for (auto matchedRule : matchedRules)
+    {
+        StyleRule* rule = matchedRule.first;
+        int specificity = matchedRule.second;
+        log(DEBUG, "getProperties: %d: %ls", specificity, rule->getKey().c_str());
+
+        for (auto prop : rule->getProperties())
+        {
+            auto it = results.find(prop.first);
+            if (it != results.end())
+            {
+                results.erase(it);
+            }
+            results.insert(make_pair(prop.first, prop.second));
+        }
+
+    }
+
+    for (auto prop : results)
+    {
+        log(DEBUG, "getProperties: %s -> %lld (0x%llx)", prop.first.c_str(), prop.second, prop.second);
+    }
+
+    return results;
+}
+
+wstring StyleRule::getKey()
+{
+    wstring key = L"";
+    if (m_widgetType.length() > 0)
+    {
+        key += m_widgetType;
+    }
+    if (m_className.length() > 0)
+    {
+        key += L"." + m_className;
+    }
+    if (m_id.length() > 0)
+    {
+        key += L"#" + m_id;
+    }
+
+    return key;
+}
+
+
